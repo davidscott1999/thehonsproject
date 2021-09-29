4d04f959638803986d26b2d5061b3f0c2feb03d3
David Scott <44474256+davidscott1999@users.noreply.github.com>
2021-09-29 13:57:03 +0100

    button default value

--- my-react-app/src/components/DropDownMenu/Button.js
+++ my-react-app/src/components/DropDownMenu/Button.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
 function DropDownMenu() {
-    const [dropdown, setDropDown] = useState('Beginner')
+    const [dropdown, setDropDown] = useState('Please Select')
 
     return (
         <form>
-            <h1> This is the value of the useState {dropdown} </h1>
+            <h1> You have selected your hillwalking experience at {dropdown} </h1>
             <select value={dropdown} onChange={(e)=>{setDropDown(e.target.value)}}>
+                <option value="Please select">Please Select</option>
                 <option value="Beginner">Beginner</option>
                 <option value="Advanced Beginner">Advanced Beginner</option>
                 <option value="Proficient">Proficient</option>
@@ -15,3 +16,5 @@ function DropDownMenu() {
         </form>
     )
 }
+
+{export DropDownMenu}
