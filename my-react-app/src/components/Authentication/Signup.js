fcc19aca9094f203a72611976df123155c1800f0
David Scott <44474256+davidscott1999@users.noreply.github.com>
2021-11-02 14:41:55 +0000

    Update Signup.js

--- my-react-app/src/components/Authentication/Signup.js
+++ my-react-app/src/components/Authentication/Signup.js
@@ -11,7 +11,7 @@ export default function Signup() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
