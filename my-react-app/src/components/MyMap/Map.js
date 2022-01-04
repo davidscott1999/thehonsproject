d3e5ecf3029a941170d303201d8676c314b813fb
David Scott <44474256+davidscott1999@users.noreply.github.com>
2022-01-04 12:30:27 +0000

    map css changes - icon for makers

--- my-react-app/src/components/MyMap/Map.js
+++ my-react-app/src/components/MyMap/Map.js
@@ -17,6 +17,7 @@ const Map = ({ className, onPopupClick }) => {
   useEffect(() => {
     const SCOTLAND = [-6, 57];
 
+    //Initializes TomTom map with an id set to the 'map' and style, zoom and center position
     const map = tt.map({
       key: process.env.REACT_APP_TOM_TOM_API_KEY,
       container: mapElement.current,
@@ -39,7 +40,7 @@ const Map = ({ className, onPopupClick }) => {
       tileSize: 256,
       minZoom: 0,
       MAX_ZOOM: 12,
-      attribution: "OpenWeatherMap.org",
+      attribution: "OpenWeatherMap",
     };
 
     //Open Weather Map API Source for Precipitation
@@ -51,7 +52,19 @@ const Map = ({ className, onPopupClick }) => {
       tileSize: 256,
       minZoom: 0,
       MAX_ZOOM: 12,
-      attribution: "OpenWeatherMap.org",
+      attribution: "OpenWeatherMap",
+    };
+
+    //Open Weather Map API Source for Wind
+    const windSource = {
+      type: "raster",
+      titles: [
+        "https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=process.env.REACT_APP_OPEN_WEATHER_KEY",
+      ],
+      tileSize: 256,
+      minZoom: 0,
+      MAX_ZOOM: 12,
+      attribution: "OpenWeatherMap",
     };
 
     //Tile Layer for Clouds
@@ -70,13 +83,23 @@ const Map = ({ className, onPopupClick }) => {
       layout: { visibility: "visible" },
     };
 
-    //Load Source & Layer for TomTom Map
+    //Tile Layer for Wind
+    const windLayer = {
+      id: "wind_layer",
+      type: "raster",
+      source: "wind_source",
+      layout: { visibility: "visible" },
+    };
+
+    //Load Source & Layer from open weather map for TomTom Map
     map.on("load", function () {
       map.addSource("cloud_source", cloudSource);
       map.addSource("rain_source", rainSource);
+      map.addSource("wind_source", windSource);
 
       map.addLayer(cloudLayer);
       map.addLayer(rainLayer);
+      map.addLayer(windLayer);
     });
 
     map.addControl(new tt.NavigationControl());
@@ -93,7 +116,15 @@ const Map = ({ className, onPopupClick }) => {
 
       new tt.Marker({
         element: markerElement,
-      }).setLngLat([latlng_lng, latlng_lat]);
+      })
+        .setLngLat([latlng_lng, latlng_lat])
+        .setPopup(
+          new tt.Popup()
+            .setOffset({ bottom: [0, -25] })
+            .setDOMContent(htmlContent)
+            .on("open", () => onPopupClick(smcid))
+        )
+        .addTo(map);
     };
 
     munroData.forEach((munroData) =>
