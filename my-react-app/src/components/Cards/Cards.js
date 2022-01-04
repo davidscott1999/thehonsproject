71e1dda881fd68497388dc1eac205c0dc3e9b620
David Scott <44474256+davidscott1999@users.noreply.github.com>
2022-01-04 12:31:34 +0000

    cards for dashboard page

--- my-react-app/src/components/Cards/Cards.js
+++ my-react-app/src/components/Cards/Cards.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React from 'react';
 
 const Cards = ({ image, children, heading }) => {
   return (
-    <div className="rounded group overflow-hidden shadow-lg bg-gray-200 hover:bg-gray-400 duration-400 pb-4">
-      <img
-        class="w-full h-56 object-cover transform group-hover:opacity-75 duration-500"
-        src={image}
-        alt="featureCards"
-      />
-      <div>
-        <div class="font-bold font-family: ui-serif m-1.5">{heading}</div>
-        <p class="text-gray-800 text-base">{children}</p>
+    <section className="w-full text-gray-900 py-20 bg-center bg-cover bg-no-repeat">
+      <div className="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-500 p-8 flex flex-col lg:ml-10 w-full mt-10 lg:mt-0 rounded-md">
+        <img
+          class="w-full h-56 object-cover transform group-hover:opacity-75 duration-500"
+          src={image}
+          alt="featureCards"
+        />
+        <div>
+          <div class="font-bold font-family: ui-serif text-centre">
+            {heading}
+          </div>
+          <p class="text-gray-800 text-base">{children}</p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
