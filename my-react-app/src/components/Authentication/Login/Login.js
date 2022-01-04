93823720c058fe5bcbf65fbe418e23a1eb0de6d2
David Scott <44474256+davidscott1999@users.noreply.github.com>
2022-01-04 12:29:55 +0000

    auth - formik and yup refactoring

--- my-react-app/src/components/Authentication/Login/Login.js
+++ my-react-app/src/components/Authentication/Login/Login.js
@@ -1,97 +1,87 @@
-import React, {
-  useRef,
-  useState,
-} from 'react';
+import {
+  ErrorMessage,
+  Field,
+  Form,
+  Formik,
+} from 'formik';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 
-import { useAuth } from '../../../contexts/AuthContext';
+const validateLogin = Yup.object().shape({
+  email: Yup.string().email("Invalid Email").required("Email is required"),
+  password: Yup.string()
+    .min(10, "Must be 10 characters or more")
+    .required("Password Required"),
+});
 
 const Login = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const Login = useAuth();
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  //prevent form refreshing
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    //error handling for failed login
-    try {
-      setError("");
-      setLoading(true);
-      await Login(emailRef.current.value, passwordRef.current.value);
-    } catch {
-      setError("Failed to sign in to your account, please try again");
-    }
-    setLoading(false);
-  }
-
   return (
-    <>
-      <section
-        className="w-full text-gray-900 py-36 bg-center bg-cover bg-no-repeat"
-        style={{
-          backgroundImage: "url(media/loginBackground.jpg)",
-          backgroundColor: "transparent",
-        }}
-      >
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
-          <div className="lg:w-3/6 lg:pr-0 pr-0">
-            <h1 className="font-large text-5xl text-white">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </h1>
-            <p className="leading-relaxed text-white mb-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
+    <section
+      className="w-full text-gray-900 py-36 bg-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: "url(media/loginBackground.jpg)",
+        backgroundColor: "transparent",
+      }}
+    >
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
+        <div className="lg:w-3/6 lg:pr-0 pr-0">
+          <h1 className="font-large text-5xl text-white">
+            Login to your Account
+          </h1>
+        </div>
+        <div className="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-200 p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={validateLogin}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log(values);
+            }}
+          >
+            {({ isSubmitting, isValid }) => (
+              <Form>
+                <label htmlFor="email">Email:</label>
+                <Field
+                  id="Email"
+                  name="email"
+                  type="email"
+                  className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
+                />
+                <div className="text-sm font-normal text-red-500 mt-1">
+                  <ErrorMessage name="email" />
+                </div>
+
+                <label htmlFor="password">Password:</label>
+                <Field
+                  id="Password"
+                  name="password"
+                  type="password"
+                  className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
+                />
+                <div className="text-sm font-normal text-red-500 mt-1">
+                  <ErrorMessage name="password" />
+                </div>
+                <button
+                  disabled={!isValid}
+                  className="text-white text-white bg-gray-600 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg w-full"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </Form>
+            )}
+          </Formik>
+          <div className="text-center mt-3">
+            <Link to="/forgotPassword">Forgot Password?</Link>
           </div>
-          <div className="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-200 p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
-            <div className="relative mb-4">
-              <label
-                for="email"
-                className="leading-7 text-sm text-gray-600"
-              ></label>
-              Email:
-              <input
-                className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
-                type="email"
-                ref={emailRef}
-                required
-              />
-            </div>
-            <div className="relative mb-4">
-              <label
-                for="password"
-                className="leading-7 text-sm text-gray-600"
-              ></label>
-              Password:
-              <input
-                className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
-                type="password"
-                ref={passwordRef}
-                required
-              />
-            </div>
-            <button
-              disabled={loading}
-              className="text-white bg-indigo-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg ml-2"
-              type="submit"
-              onSubmit={handleSubmit}
-            >
-              Login
-            </button>
-            {error && <alert variant="danger">{error}</alert>}
-            <div className="text-center mt-3">
-              <a href="/forgotpassword">Forgot Password?</a>
-            </div>
-            <div className="text-center mt-3">
-              <a href="/signup">Not registered for an account?</a>
-            </div>
+          <div className="text-center mt-3">
+            <Link to="/signup">Not registered for an account?</Link>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
