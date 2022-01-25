c39c17b50d620ebd9b84d130a2ff4bc1923a3941
David Scott <44474256+davidscott1999@users.noreply.github.com>
2022-01-25 13:44:57 +0000

    munro profile

--- my-react-app/src/components/App/App.js
+++ my-react-app/src/components/App/App.js
@@ -14,9 +14,14 @@ import { UpdatePassword } from '../Authentication/Account/updatePassword';
 import { Login } from '../Authentication/Login/Login';
 import { ResetPassword } from '../Authentication/PasswordReset/PasswordReset';
 import { Signup } from '../Authentication/Signup';
+import ErrorPage from '../Error/ErrorPage';
+import { FeatureCards } from '../FeatureCards/FeatureCards';
 import Footer from '../Footer/Footer';
+import { MunroDetails } from '../MunroDetails/MunroDetails';
+import { MunroProfile } from '../MunroDetails/MunroProfile';
 import { Map } from '../MyMap/Map';
 import Navbar from '../Navbar/Navbar';
+import VerifyAccount from '../Verify/VerifyAccount';
 
 function App() {
   const [munro, setMunro] = useState();
@@ -31,14 +36,20 @@ function App() {
             <Route exact path="/">
               <div className="relative">
                 <Map className="map" onPopupClick={setMunro} />
-                {munro && <munroData className="munroData" munroId={munro} />}
+                {munro && (
+                  <MunroDetails className="munroDetails" munroId={munro} />
+                )}
+                <FeatureCards className="max-w-desktop" />
               </div>
             </Route>
+            <Route path="/munros" munroId={munro} component={MunroProfile} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route path="/resetpassword" component={ResetPassword} />
             <Route path="/details" component={UpdateEmail} />
             <Route path="/changepassword" component={UpdatePassword} />
+            <Route path="/error" component={ErrorPage} />
+            <Route path="/verify" component={VerifyAccount} />
           </Switch>
           <Footer />
         </div>
