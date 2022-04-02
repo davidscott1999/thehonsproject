caf8a1723f02bf3b42bf3da96d8bf3a740fc8bb2
David Scott <44474256+davidscott1999@users.noreply.github.com>
2022-04-02 22:44:58 +0100

    updating test

--- my-react-app/src/components/MunroLogs/MunroLogs.test.js
+++ my-react-app/src/components/MunroLogs/MunroLogs.test.js
@@ -17,11 +17,13 @@ test("submitting the form calls onSubmit with munro details", () => {
   const region = "Loch Linnhe to Loch Ericht";
   const date = "12/02/2022";
   const comment = "Test";
+  const file = "munrotest.png";
   userEvent.type(screen.getByText(/name:/i), name);
   userEvent.type(screen.getByText(/smcid:/i), smcid);
   userEvent.type(screen.getByText(/region:/i), region);
   userEvent.type(getByText(/date:/i), date);
   userEvent.type(getByText(/comment:/i), comment);
+  userEvent.type(getByText(/file:/i), file);
   userEvent.type(
     screen.getByRole("button", {
       name: /save/i,
